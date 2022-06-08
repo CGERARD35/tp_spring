d7d2686e6930fa1380d2b20874c153f5a244f6d2
Gurvan Mansanarez <gurvan.mansanarez@epsi.fr>
2022-06-08 16:04:37 +0200

    Columns, Table of User and Vehicle

---
+++ src/main/java/projetPOEIspring/poeidata/models/Vehicle.java
@@ -0,0 +1,44 @@
+package projetPOEIspring.poeidata.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "vehicle")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Vehicle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 30, nullable = false)
+    private String plateNumber;
+
+    @Column(length = 30, nullable = false)
+    private String brand;
+
+    @Column(length = 5, nullable = false)
+    private String yearOfConstruction;
+
+    @OneToOne
+    private Technician technician;
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "id=" + id +
+                ", plateNumber='" + plateNumber + '\'' +
+                ", brand='" + brand + '\'' +
+                ", yearOfConstruction='" + yearOfConstruction + '\'' +
+                ", technician=" + technician +
+                '}';
+    }
+}
