1d376ec10cf10218e31e147ccea1a21f4edb891f
Carl GERARD <carl.gerard@epsi.fr>
2022-06-08 16:12:25 +0200

    Ajouts attributs + relations Technician

--- src/main/java/projetPOEIspring/poeidata/models/Technician.java
+++ src/main/java/projetPOEIspring/poeidata/models/Technician.java
@@ -1,9 +1,18 @@
 package projetPOEIspring.poeidata.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "Technician")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Technician {
 
     @Id
@@ -12,5 +21,23 @@ public class Technician {
 
     @Column(nullable = false)
     private String firstname;
+
+    @Column(nullable = false)
+    private String lastname;
+
+    @Column(nullable = false)
+    private Integer age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "manager_id", nullable = false)
+    private Manager manager;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "adress_id", nullable = false)
+    private Adress adress;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "worksite_id", nullable = false)
+    private Worksite worksite;
 
 }
