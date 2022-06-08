0a49c12deb447dc4985d8464938066c24aaab972
Carl GERARD <carl.gerard@epsi.fr>
2022-06-08 16:12:37 +0200

    Ajouts attributs + relations Manager

--- src/main/java/projetPOEIspring/poeidata/models/Manager.java
+++ src/main/java/projetPOEIspring/poeidata/models/Manager.java
@@ -1,4 +1,39 @@
 package projetPOEIspring.poeidata.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "manager")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Manager {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String firstname;
+
+    @Column(nullable = false)
+    private String lastname;
+
+    @Column(nullable = false)
+    private String Phone;
+
+    @Column(name = "mobile_phone",
+            nullable = false)
+    private String mobilePhone;
+
+    @OneToMany (mappedBy = "manager")
+    private Set<Technician> technician;
+
 }
