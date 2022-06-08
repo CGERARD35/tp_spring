1956178f3e37848f435b8838d7ad2d0cfa913b0d
Carl GERARD <carl.gerard@epsi.fr>
2022-06-08 14:26:06 +0200

    first commit

---
+++ src/main/java/projetPOEIspring/poeidata/ProjetPoeiSpringApplication.java
@@ -0,0 +1,15 @@
+package projetPOEIspring.poeidata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetPoeiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetPoeiSpringApplication.class, args);
+		System.out.println("hello");
+		System.out.println("Test push Gurvan");
+	}
+
+}
