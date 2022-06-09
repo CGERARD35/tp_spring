03adca58018aafa935cb44d10e63e1ad79083bc1
TheronBenjamin <benjamin.theron.bt@gmail.com>
2022-06-09 11:36:16 +0200

    WorksiteServiceImpl + technician many to many

--- src/main/java/projetPOEIspring/poeidata/mappers/ManagerMapper.java
+++ src/main/java/projetPOEIspring/poeidata/mappers/ManagerMapper.java
@@ -9,6 +9,7 @@ import projetPOEIspring.poeidata.api.dto.TechnicianDto;
 import projetPOEIspring.poeidata.models.Adress;
 import projetPOEIspring.poeidata.models.Manager;
 import projetPOEIspring.poeidata.models.Vehicle;
+import projetPOEIspring.poeidata.models.Worksite;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,7 +21,7 @@ public interface ManagerMapper {
 //    @Mapping(target = "technicians", expression = "java(getTechnician(manager))")
     ManagerDto mapManagerToManagerDto(Manager manager);
 
-    default List<TechnicianDto> getTechnicians(Manager manager, Adress adress, Vehicle vehicle) {
+    default List<TechnicianDto> getTechnicians(Manager manager, Adress adress, Vehicle vehicle, Worksite worksite) {
         List<TechnicianDto> technicians = new ArrayList<>();
         if (manager.getTechnicians() != null) {
             technicians = manager.getTechnicians().stream()
@@ -31,7 +32,8 @@ public interface ManagerMapper {
                             technician.getAge(),
                             manager.getId(),
                             adress.getId(),
-                            vehicle.getId()
+                            vehicle.getId(),
+                            technician.getWorksites().stream().map(Worksite::getId).toList()
                     ))
                     .toList();
         }
--- src/main/java/projetPOEIspring/poeidata/models/Technician.java
+++ src/main/java/projetPOEIspring/poeidata/models/Technician.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -37,10 +38,10 @@ public class Technician {
     @JoinColumn(name = "adress_id")
     private Adress adress;
 
-    @ManyToMany(cascade = {CascadeType.ALL})
+    @ManyToMany(cascade = {CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
     @JoinTable(joinColumns={@JoinColumn(name="technician_id")},
             inverseJoinColumns = {@JoinColumn(name="worksite_id")})
-    private Set<Worksite> worksites;
+    private Set<Worksite> worksites = new HashSet<>();
 
     @OneToOne
     @JoinColumn(name = "vehicle_id")
